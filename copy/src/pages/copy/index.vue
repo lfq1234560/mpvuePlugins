cb4fe961c08717808b65ff25825da27f0021813b
lfq1234560 <lfq940702>
2021-03-01 10:02:34 +0800

    复制功能

---
+++ copy/src/pages/copy/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="copy">
+    <p>{{url}}</p>
+    <button @click="copy">点我复制</button> 
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data () {
+    return {
+      //百度网址
+      url:"http://baidu.com",
+    }
+  },
+  methods:{
+    /**
+     * description:复制功能
+     */
+    copy(){
+      //调用设置剪切板api
+      wx.setClipboardData({
+        data: this.url,
+        success: (result) => {
+          
+        },
+        fail: () => {},
+        complete: () => {}
+      });
+        
+    }
+  }
+
+}
+</script>
+
+<style soped>
+p{
+  height: 50rpx;
+  line-height: 50rpx;
+  text-align: center;
+  background:blue;
+}
+
+</style>
